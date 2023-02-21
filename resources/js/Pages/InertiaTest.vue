a9525504e1b3929b3d952089ac64c6855b97fea7
ken0579jp <ken0579jp@gmail.com>
2023-02-21 17:36:40 +0900

    Feb.22

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+  import { Link } from '@inertiajs/vue3';
+  import { ref } from 'vue'
+
+  const newTitle = ref('');
+  const newContent = ref('');
+
+</script>
+
+<template>
+  Inertiaテストです。<br>
+  <a href="/">aタグ経由です</a><br>
+  <Link href="/">link経由です</Link><br>
+  <Link :href="route('inertia.index')">名前付きルート経由です</Link><br>
+  <Link :href="route('inertia.show', { id: 50 })">ルートパラメータのテストです</Link><br>
+
+  <div class="mb-8"><br><br></div>
+
+  <input type="text" name="newTitle" v-model="newTitle">{{ newTitle }}<br>
+  <input type="text" name="newContent" v-model="newContent">{{ newContent }}<br>
+
+  <Link as="button" method="POST" :href="route('inertia.store')"
+  :data="{
+    title: newTitle,
+    content: newContent
+  }"
+  >DB保存テスト</Link>
+
+
+</template>
